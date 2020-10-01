ccb34555075832317908d53d5ad4674f0978c219
jemeson007 <emesonjonah@gmail.com>
2020-10-01 02:35:51 -0700

    Update Ar.java
    Done run

--- Ar.java
+++ Ar.java
@@ -1,67 +1,68 @@
-import java.util.*;
 
-class Ar{
-  public static int Yearn(int age, int spacetime){
+import java.util.HashMap;
+import java.util.Scanner;
 
-  int ability = age ** spacetime;
-  int solve = Integer.valueOf(log(ability));
-
-}
-
-  public static String Dose(){
-  
+public class Ar {
   
 
-  HashMap<int, String> Vaccine = new HashMap<int, String>();
-  Vaccine.put(1, "Fiduciary);
-  Vaccine.put(2, "Honesty");
-  Vaccine.put(3, "Halicin");
- 
- 
-  switch(solve){
-    case 1:
-      System.out.println(Vaccine.get(1));
-      break;
+	  public static int Yearn(int a, int spacet){
 
-    case 2:
-      System.out.println(Vaccine.get(2));
-      break;
+	  int ability = (int)(Math.pow(a, spacet));
+	  int solve = (int)(Math.log(ability));
+	 
+	 
 
-    case 3:
-      System.out.println(Vaccine.get(3));
-      break;
+	  HashMap<Integer, String> Vaccine = new HashMap<Integer, String>();
+	  Vaccine.put(1, "Fiduciary");
+	  Vaccine.put(2, "Honesty");
+	  Vaccine.put(3, "Halicin");
+	 
+	 
+	  switch(solve){
+	    case 1:
+	        System.out.println(Vaccine.get(1));
+	      break;
+	    case 2:
+	        System.out.println(Vaccine.get(2));
+	      break;
+	    case 3:
+	       System.out.println(Vaccine.get(3));
+	      break;
+	    default:
+	       System.out.println("another");
+	      break;
+	  }
+	 
+	  return solve;
+	 
+	 
+	}
+	//   public static boolean Heal(){
+//	     if (solve == 1 || solve == 2 || solve == 3)
+//	       return true;
+//	     else
+//	       return false;
+	//   }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int spacetime = 93000000;
+	    try (Scanner sy = new Scanner(System.in)) {
+			System.out.println("Enter age: ");
+			int age = sy.nextInt();
+   
+   
+			// BufferedReader red = new BufferedReader(new InputStreamReader(System.in));
+   
+			// System.out.println("Enter age: ");
+			// int age = red.read();
+   
+			Yearn(age, spacetime);
+			//Dose();
+			// Heal();
+		}
+	 
 
-    default:
-      System.out.println("another");
-      continue
-  }
- 
+	}
 
 }
-
-  public static boolean Heal(){
-    if (solve == 1 || solve == 2 || solve ==3)
-      return true;
-    else
-      return false;
-  }
-
-  public static void main(String [] args){
-
-    int spacetime = 93000000;
-
-    Scanner sy = new Scanner(System.in());
-
-    System.out.println("Enter age");
-    int age = sy.nextInt();
-
-    Yearn(age, spacetime);
-    Dose();
-    Heal();
-  
-     
-    
-  }
-
-}
-
